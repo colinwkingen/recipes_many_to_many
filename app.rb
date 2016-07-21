e64a6e58cea2f172405ea74e301c0085665f74e2
Colin W Kingen and Matt Carlson <colin+matt@>
2016-07-21 09:09:15 -0700

    fix duplicate object bug

--- app.rb
+++ app.rb
@@ -33,26 +33,63 @@ get('/categories/:id') do
   erb(:category)
 end
 
+# post('/recipes/:id/ingredients') do
+#   @recipe = Recipe.find(params['id'].to_i)
+#   name = params['name']
+#   if Ingredient.exists?(name) == false
+#     new_ingredient = Ingredient.create({name: name})
+#     @recipe.ingredients.push(new_ingredient)
+#   else
+#     found_ingredient = Ingredient.find_by_name(name)
+#     if Ingredient.find_by_name(found_ingredient.name()) == found_ingredient
+#       @recipe.ingredients.push(found_ingredient)
+#     end
+#   end
+#   redirect to ('/recipes/' + @recipe.id().to_s)
+# end
+
 post('/recipes/:id/ingredients') do
   @recipe = Recipe.find(params['id'].to_i)
-  name = params['name']
-  if Ingredient.exists?(name) == false
-    new_ingredient = Ingredient.create({name: name})
-    @recipe.ingredients.push(new_ingredient)
-  else
-    found_ingredient = Ingredient.find_by_name(name)
-    if Ingredient.find_by_name(found_ingredient.name()) == found_ingredient
-      @recipe.ingredients.push(found_ingredient)
+  ingredient = params['name']
+  new_ingredient = Ingredient.find_or_create_by(name: ingredient)
+  if @recipe.ingredients.length != 0
+    found = nil
+    @recipe.ingredients.each do |ingredient|
+      if ingredient.name() == new_ingredient.name()
+        next
+      else
+        found = true
+      end
+      if found == true
+        @recipe.ingredients.push(new_ingredient)
+      end
     end
+  else
+    @recipe.ingredients.push(new_ingredient)
   end
   redirect to ('/recipes/' + @recipe.id().to_s)
 end
+
 
 post('/recipes/:id/categories') do
   @recipe = Recipe.find(params['id'].to_i)
   category = params['category']
   new_category = Category.find_or_create_by(name: category)
-  @recipe.categories.push(new_category)
+  if @recipe.categories.length != 0
+    found = nil
+    @recipe.categories.each do |category|
+      if category.name() == new_category.name()
+        next
+      else
+        found = true
+      end
+      if found == true
+        @recipe.categories.push(new_category)
+      end
+    end
+  else
+    @recipe.categories.push(new_category)
+  end
   redirect to ('/recipes/' + @recipe.id().to_s)
 end
 
